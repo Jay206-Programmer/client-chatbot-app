a8f332f01180f2951664e500175464ccfa721490
Jay Shukla <jshukla@infosenseglobal.com>
2021-10-08 00:02:26 -0700

    font changes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         config={config}
         messageParser={MessageParser}
         actionProvider={ActionProvider}
+        headerText='Database Assistant'
       />
       {/* </header> */}
     </div>
