e6ed7886111790e21c292d2f7dc0836af2a33d39
Jay Shukla <jshukla@infosenseglobal.com>
2021-10-08 09:23:04 -0700

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
 import "./App.css";
+import cb_logo from "./1540869642.png";
 
 import config from "./bot/config.js";
 import MessageParser from "./bot/MessageParser.js";
@@ -9,13 +10,23 @@ import ActionProvider from "./bot/ActionProvider.js";
 function App() {
   return (
     <div class="main-container">
-      <div class="sidebar">Hi</div>
+      <div class="sidebar">
+        <div class="intro-container">
+          {/* <div class="image-container" style={}>
+            <img src={cb_logo} alt="" width="50%" />
+          </div> */}
+          <div>
+            <h3>Database Assistant</h3>
+          </div>
+
+        </div>
+      </div>
       {/* <header className="App-header"> */}
       <Chatbot
         config={config}
         messageParser={MessageParser}
         actionProvider={ActionProvider}
-        headerText='Database Assistant'
+        headerText="Database Assistant"
       />
       {/* </header> */}
     </div>
